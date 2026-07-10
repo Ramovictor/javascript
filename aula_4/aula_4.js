cea5a21dfca399250d222b98e7bf984a104da8ac
Ramon Victor <ramonvictor.0003gmai.com>
2026-07-09 22:00:26 -0300

    Aula JS

--- aula_4/aula_4.js
+++ aula_4/aula_4.js
@@ -1,10 +1,16 @@
 async function buscarCep() {
-  const cepDigitado = document.querySelector("#cep").value;
-  const url = `https://viacep.com.br/ws/${cepDigitado}/json/`;
+  const cepDigitado = document.querySelector("#cep").value; //PEga oque ta digitado naquele momento
+  const url = `https://viacep.com.br/ws/${cepDigitado}/json/`; //URL da API do JSON
 
-  const resposta = await fetch(url);
-  const dados = await resposta.json();
+  const resposta = await fetch(url); //Request/Requisição(chamando o garçom)
+  const dados = await resposta.json(); //
 
-  console.log(dados);
+  popularInputs(dados);
 }
-function popularInputs(dados) {}
+function popularInputs(dados) {
+  document.querySelector("#logradouro").value = dados.logradouro;
+  document.querySelector("#cidade").value = dados.localidade;
+  document.querySelector("#bairro").value = dados.bairro;
+  document.querySelector("#uf").value = dados.uf;
+  document.querySelector("#regiao").value = dados.regiao;
+}
